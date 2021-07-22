f5dad9f4628e27d0937f8350429e6c7ebedb13f4
Javaun Salmon <misterjavaun@gmail.com>
2021-07-22 09:41:04 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,136 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import instagramLogo from "./insta.png";
+import youtubeLogo from "./yt.png";
 import "./App.css"
 
-class LambdaDemo extends Component {
+class Home extends Component {
   constructor(props) {
     super(props)
-    this.state = { loading: false, msg: null }
+    this.state = { loading: false, msg: null, page: 'Home' }
   }
 
-  handleClick = api => e => {
-    e.preventDefault()
+  buttonGroup = () =>{
+    return(
+      <div style={{marginTop: '5vw', lineHeight:2}}>
+        <a className='Home-Button-Group' href='javascript:;' style={{ borderColor: 'orange', color: 'orange', backgroundColor: 'white'}} onClick={this.goToPage("Home")}>Home</a>
+        <a className='Home-Button-Group' href='javascript:;' style={{ borderColor: 'orange', color: 'orange', backgroundColor: 'white'}} onClick={this.goToPage("About")}>About Me</a>
+        <a className='Home-Button-Group' href='javascript:;' style={{ borderColor: 'orange', color: 'orange', backgroundColor: 'white'}} onClick={this.goToPage("Portfolio")}>Portfolio</a>
+        <a className='Home-Button-Group' href='javascript:;' style={{ borderColor: 'orange', color: 'orange', backgroundColor: 'white'}} onClick={(e) => { e.preventDefault(); window.open('https://docs.google.com/document/d/1AsrEufq9V44RcDLCTWm0pLo07HXJIzdS/edit', "_blank")}}>Resume</a>
+        <a className='Home-Button-Group' href='javascript:;' style={{ borderColor: 'orange', color: 'orange', backgroundColor: 'white'}} onClick={this.goToPage("Contact")}>Contact</a>
+      </div>
+    )
+  }
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
+  goToPage = name => e => {
+    e.preventDefault()
+    this.setState({page: name})
   }
 
   render() {
     const { loading, msg } = this.state
+    if(this.state.page == 'Home'){
+      return (
+        <div className="App" style={{ backgroundImage: `url(${require("./bkg.jpg")})`}}>
+            <div className='Intro-Text'>
+              <h1 className='Regular header-text'>Hi. My name is <span className='Bold' style={{color: 'orange'}}>Javaun Salmon</span>!</h1>.
+              <div className="Sub-Intro-Text">
+                <h3 className='Bold'>Degree: <span className='Regular'>Bachelors of Science</span></h3>
+                <h3 className='Bold'>Study: <span className='Regular'>Management Information Systems</span></h3>
+                <h3 className='Bold'>School: <span className='Regular'><a className='School-Link' style={{color: 'orange'}} href="https://www.rit.edu/" target="_blank">Rochester Institute of Technology</a></span></h3>
+              </div>
+              {this.buttonGroup()}
+          </div>
+      </div>
+      )
+    }
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
+    if(this.state.page == 'About'){
+      return(
+        <div className="App" style={{ backgroundImage: `url(${require("./bkg.jpg")})`}}>
+            <div className='Intro-Text'>
+              <h1 className='header-text' style={{color: 'orange', fontFamily: 'Righteous'}}>About Me</h1>.
+              <div className="Sub-Intro-Text" style={{marginLeft: '20vw', marginRight: '20vw'}}>
+                <p style={{fontSize: '18px'}} >
+                  Hi! I am an entrepreneur and software developer interested in using technology to improve business processes. My goal as a software developer is to write clean, easily maintained code. I enjoy thinking creatively and critically in order to design efficient software. In my free time, I like to sharpen my skills and learn new technologies. I work best in collaborative environments where a growth-mindset is emphasized and is at the core of every project.
+                </p>
+              </div>
+
+            {this.buttonGroup()}
+          </div>
+      </div>
+      )
+    }
+
+    if(this.state.page == 'Portfolio'){
+      return(
+        <div className="App" style={{ backgroundImage: `url(${require("./bkg.jpg")})`}}>
+            <div className='Intro-Text'>
+              <h1 className='header-text' style={{color: 'orange', fontFamily: 'Righteous'}}>Portfolio</h1>.
+              <div className='grid-container'>
+                <div>
+                  <a href="https://www.instagram.com/javaun.salmon.apps/" target="_blank"><img src={instagramLogo} style={{width: '100px', height: '100px'}}/></a>
+                  <div>
+                    <h2 style={{fontSize: '25px', fontFamily: 'Righteous'}}>
+                      JS Apps
+                    </h2>
+                    <p style={{fontSize: '18px'}}>
+                      This is my instagram page built to showcase my freelance projects with a recording and a brief overview of the project.
+                    </p>
+                  </div>
+                </div>
+
+                <div>
+                  <a href="https://www.youtube.com/channel/UC36Ra4RhmGGSpmFaE4cb2Tw/featured" target="_blank"><img src={youtubeLogo} style={{width: '100px', height: '100px'}}/></a>
+                  <div>
+                    <h2 style={{fontSize: '25px', fontFamily: 'Righteous'}}>
+                      My Youtube Channel
+                    </h2>
+                    <p style={{fontSize: '18px'}}>
+                      This is my youtube channel. It was created to show viewers how to build a basic animation application using the processing js library.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            {this.buttonGroup()}
+          </div>
+      </div>
+      )
+    }
+
+    if(this.state.page == 'Contact'){
+      return(
+        <div className="App" style={{ backgroundImage: `url(${require("./bkg.jpg")})`}}>
+            <div className='Intro-Text'>
+              <h1 className='header-text' style={{color: 'orange', fontFamily: 'Righteous'}}>Let's Chat!</h1>.
+              <div className="Sub-Intro-Text" style={{marginLeft: '20vw', marginRight: '20vw'}}>
+                <p style={{fontSize: '16px'}}>
+                  <strong>Phone:</strong> <a href="tel:6465412356" style={{color: 'orange'}}>(646)-541-2356</a>
+                </p>
+
+                <p style={{fontSize: '16px'}}>
+                  <strong>Email:</strong> <a href="mailto:js1463@g.rit.edu" style={{color: 'orange'}}>js1463@g.rit.edu</a>
+                </p>
+
+                <p style={{fontSize: '16px'}}>
+                  <strong>linkedin:</strong> <a href='https://www.linkedin.com/in/javaun-salmon-77944489/' style={{color: 'orange'}}>Javaun Salmon</a>
+                </p>
+              </div>
+
+            {this.buttonGroup()}
+          </div>
+      </div>
+      )
+    }
   }
 }
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-           Ready to go now
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
+
+            <Home />
+
     )
   }
 }
